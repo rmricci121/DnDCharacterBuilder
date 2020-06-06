83e808dc495e6603c0bca923265f33897f3ff373
rmricci121 <rmricci121@gmail.com>
2020-06-05 20:03:23 -0400

    Added user & charcter model

---
+++ db/migrate/20200606000250_create_characters.rb
@@ -0,0 +1,11 @@
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :race
+      t.string :class
+
+      t.timestamps
+    end
+  end
+end
