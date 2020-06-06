739a8e55dca6878a4ec95aaff1e71f6fcbb831b0
rmricci121 <rmricci121@gmail.com>
2020-06-06 09:56:39 -0400

    Added MIT license

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(id: params[:id])
-     session[:user_id] = @user.id
-     
-       #redirect_to user_path(@user)
-    
-    
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    else
+        render :new
+    end
   end
+    
+    
+
 
   def destroy
     session.delete :user_id
