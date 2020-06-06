26cdc9d4a0ea0b064b2d90070ae8cbd0e5c18285
rmricci121 <rmricci121@gmail.com>
2020-06-05 20:25:23 -0400

    adjusted a type-o in my campiagns table and ran migration

--- db/migrate/20200606000846_create_campaigns.rb
+++ db/migrate/20200606000846_create_campaigns.rb
@@ -2,8 +2,8 @@ class CreateCampaigns < ActiveRecord::Migration[6.0]
   def change
     create_table :campaigns do |t|
       t.string :name
-      t.character_id :integer
-      t.adventure_id :integer
+      t.integer :character_id
+      t.integer :adventure_id
 
       t.timestamps
     end
