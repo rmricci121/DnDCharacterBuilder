d9b8318ead4cf3965ca1c2e81fa347fd5afe389a
rmricci121 <rmricci121@gmail.com>
2020-06-05 20:21:03 -0400

    added resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  get 'adventure/new'
-  get 'adventure/show'
-  get 'adventure/index'
-  get 'campaign/new'
-  get 'campaign/show'
-  get 'campaign/index'
-  get 'character/new'
-  get 'character/show'
-  get 'character/index'
-  get 'sessions/new'
-  get 'sessions/show'
-  get 'sessions/index'
-  get 'user/new'
-  get 'user/show'
+  # get 'adventure/new'
+  # get 'adventure/show'
+  # get 'adventure/index'
+  # get 'campaign/new'
+  # get 'campaign/show'
+  # get 'campaign/index'
+  # get 'character/new'
+  # get 'character/show'
+  # get 'character/index'
+  # get 'sessions/new'
+  # get 'sessions/show'
+  # get 'sessions/index'
+  # get 'user/new'
+  # get 'user/show'
+
+  resources :user
+  resources :character
+  resources :campaign
+  resources :adventure
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
