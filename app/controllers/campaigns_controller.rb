e7a9af63d9e9e1de700645414eb30a2616dc619f
rmricci121 <rmricci121@gmail.com>
2020-06-06 11:36:56 -0400

    Added new campaign view

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,5 +1,10 @@
 class CampaignsController < ApplicationController
   def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.create(name: params[:name])
   end
 
   def show
